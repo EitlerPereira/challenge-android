f2ffe3c0cf819dcad13afc788ed65b7d0b64bab3
EitlerPereira <eitler.pereira@gmail.com>
2019-06-29 18:26:12 -0300

    Adjustment basic project

---
+++ app/src/main/kotlin/ga/eitler/challenge_android/DetailActivity.kt
@@ -0,0 +1,16 @@
+package ga.eitler.challenge_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+}
